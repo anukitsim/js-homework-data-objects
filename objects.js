ec876c778adfa2170a950cd8ccfa7cb918d4f02c
anukitsim <104069419+anukitsim@users.noreply.github.com>
2022-06-10 17:02:50 +0400

    done

---
+++ objects.js
@@ -0,0 +1,50 @@
+var fruit = ["ვაშლი", "მსხალი" , "ატამი"];
+
+// fruit.pop ();
+
+// fruit.pop ();
+
+// fruit.pop ();
+
+// fruit.push ("ატამი");
+
+// fruit.push ("მსხალი");
+
+// fruit.push ("ვაშლი");
+
+// document.write(fruit);
+
+
+// document.write(fruit.reverse() + "</br>");
+
+for (var i = fruit.length - 1; i >=0 ; i--){
+    document.write(fruit[i] + " ,");
+
+}
+
+
+
+
+
+
+
+// var welcome;
+
+// var season = new Date ();
+
+// var day = season.getDay();
+
+// var month = season.getMonth ();
+
+// if ( month <= 4 || month >= 10 && day === 0 || day === 5 || day ===6){
+//     welcome = " ცივა, წავიდეთ საგუნდაოთ ან სათხილამუროთ";
+
+// }
+
+// else if (month <=5 || month <= 9 && day === 0 || day === 5 || day ===6)  {
+//     welcome = " ცხელა, წავიდეთ ზღვაზე";
+// }
+
+// document.write(welcome+month+day);
+
+
